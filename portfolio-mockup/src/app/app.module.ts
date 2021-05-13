03f35e5dfffa0dfbc7844d00a047fa429ba0f718
IgnaBiodre <ignacio.d.mendez@gmail.com>
2021-05-13 19:27:13 -0300

    unfinishedabout

---
+++ portfolio-mockup/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+   ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
