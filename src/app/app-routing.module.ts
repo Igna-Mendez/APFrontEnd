3694f596520c1cefead5312b87fcd7d67979d218
IgnaBiodre <ignacio.d.mendez@gmail.com>
2022-07-11 19:36:12 -0300

    carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/signin/signin.component';
 import { RegisterComponent } from './components/register/register.component';
-
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FormacionComponent } from './formacion/formacion.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
  /*  { path: '', pathMatch: 'full', redirectTo: 'signin' }, */
   { path: 'signin', component: SigninComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'Experience', component : ExperienciaComponent},
+  { path: 'Proyects', component : ProyectosComponent },
+  { path: 'Education', component : FormacionComponent },
+  { path: 'About', component : AboutComponent }
 ];
 
 @NgModule({
