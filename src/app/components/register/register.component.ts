9c304cd7c70116aebc110fe38f4ad22e7c105a80
IgnaBiodre <ignacio.d.mendez@gmail.com>
2022-07-15 20:11:54 -0300

    updatedfront

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { SigninComponent } from '../signin/signin.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -9,14 +11,21 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class RegisterComponent implements OnInit {
 
   User: any = ['Super Admin', 'Author', 'Reader'];
-  constructor( private matDialogRef: MatDialogRef<RegisterComponent> ) { }
-   
+  constructor(private matDialogRef: MatDialogRef<RegisterComponent>, public dialog: MatDialog) { }
+
+ 
+
+  openDialog() : void {
+
+    this.dialog.open(SigninComponent, {})
+  }
+
   ngOnInit(): void {
   }
 
   register() {
-    /* on succes */ 
-    this.matDialogRef.close({ success:true })
+    /* on succes */
+    this.matDialogRef.close({ success: true })
   }
 
 }
